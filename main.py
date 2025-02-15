4c258946376eab6fa6e2355b528425a2d709d510
liam <clink.6@osu.edu>
2025-02-14 20:45:38 -0500

    Made 2D plot

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.fft import fft, ifft, fftshift, ifftshift, fftfreq
 
-WIDTH = 10.0e-6
-GRID_N = 2**9
+WIDTH = 5.0e-6
+GRID_N = 2**10
 WAVELENGTH = 1.0e-6  # m
 SPATIAL_FREQUENCY = 1.0 / WAVELENGTH  # rad / m
 print(SPATIAL_FREQUENCY)
 
-x = np.linspace(-100.0e-6, 100.0e-6, GRID_N)
-y = np.linspace(0.0, 200.0e-6, GRID_N)
+x = np.linspace(-25.0e-6, 25.0e-6, GRID_N)
+y = np.linspace(0.0, 50.0e-6, GRID_N)
 
 X, Y = np.meshgrid(x, y, indexing="ij")
 
@@ -35,17 +35,20 @@ propagator = np.ones_like(f_initial_amplitude)
 # TODO: Do I need a 2pi factor in frequency space?
 mask_propagating = np.abs(frequencies) <= SPATIAL_FREQUENCY
 propagator[mask_propagating] = (
-    np.exp(-1.0j * dy * np.sqrt(SPATIAL_FREQUENCY**2 - frequencies**2))
+    np.exp(-1.0j * 2.0 * np.pi * dy * np.sqrt(SPATIAL_FREQUENCY**2 - frequencies**2))
 )[mask_propagating]
 mask_evanescent = np.abs(frequencies) > SPATIAL_FREQUENCY
 propagator[np.abs(frequencies) > SPATIAL_FREQUENCY] = (
-    np.exp(-dy * np.sqrt(-(SPATIAL_FREQUENCY**2) + frequencies**2))
+    np.exp(-2.0 * np.pi * dy * np.sqrt(-(SPATIAL_FREQUENCY**2) + frequencies**2))
 )[mask_evanescent]
 
-
-f_new_amplitude = f_initial_amplitude * propagator
-new_amplitude = ifft(f_new_amplitude)
-plt.plot(x, np.abs(new_amplitude))
-plt.show()
-
 result = np.zeros_like(X)
+f_old_amplitude = f_initial_amplitude
+for j, _ in enumerate(y):
+    f_new_amplitude = f_old_amplitude * propagator
+    result[:, j] = ifft(f_new_amplitude)
+    f_old_amplitude = f_new_amplitude
+
+plt.pcolormesh(X, Y, np.abs(result) ** 2)
+plt.gca().set_aspect("equal")
+plt.show()
